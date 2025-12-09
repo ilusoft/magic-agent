ce75df092ac80e55834351318152daac1297235a
Jorge Luna <jorge@pricerqx.com>
2025-12-09 14:53:00 -0500

    Backend Bootstrap review

--- backend/src/MagicAgent.Api/Program.cs
+++ backend/src/MagicAgent.Api/Program.cs
@@ -10,64 +10,72 @@ using PRQXCommon.Core.Configuration;
 using PRQXCommon.Core.Cors;
 using PRQXCommon.Core.Enums;
 using PRQXCommon.Core.HealthCheck;
+using PRQXCommon.Core.Logging;
 using PRQXCommon.Core.Swagger;
 using PRQXCommon.Core.Versioning;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.AddPrqxConfiguration(builderType: BuilderType.WebAppBff,
-    sc =>
-    {
-        sc.AddPrqxOption<AzureAdSettings>();
-        sc.AddPrqxOption<BffSettings>();
-    });
-
-builder.Services
-    .AddProblemDetails();
-
-builder.Services.AddPrqxApiVersioning();
-builder.Services.AddPrqxSwagger("Magic Agent API");
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options =>
-    {
-        if (!options.JsonSerializerOptions.Converters.Any(converter => converter is JsonStringEnumConverter))
-        {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
-        }
-    });
-
-builder.Services.AddHttpContextAccessor();
-
-//builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddPrqxAuthentication();
-builder.Services.AddPrqxAuthorization(BuilderType.WebAppBff);
-builder.Services.AddPrqxHealthChecks();
-builder.Services.AddPrqxCors();
-
-builder.Services.Configure<AgentDefinitionsOptions>(builder.Configuration.GetSection("AgentDefinitions"));
-builder.Services.AddSingleton<IAgentDefinitionsProvider, FileAgentDefinitionsProvider>();
-builder.Services.AddSingleton<IAgentDefinitionValueResolver, AgentDefinitionConfigurationResolver>();
-builder.Services.AddSingleton<IAgentConversationStore, InMemoryAgentConversationStore>();
-builder.Services.AddSingleton<IAgentDiagnosticsStore, InMemoryAgentDiagnosticsStore>();
-builder.Services.AddSingleton<IAgentRunProgressSink, NoOpAgentRunProgressSink>();
-builder.Services.AddSingleton<IAgentRunner, DefaultAgentRunner>();
-builder.Services.AddWorkflowExpressionServices();
-
-var app = builder.Build();
-app.UsePrqxConfiguration();
-app.UsePrqxHealthChecks();
-if (app.Environment.IsDevelopment())
+try
 {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.AddPrqxConfiguration(builderType: BuilderType.WebAppBff,
+        sc =>
+        {
+            sc.AddPrqxOption<AzureAdSettings>();
+            sc.AddPrqxOption<BffSettings>();
+        });
+
+    builder.Host.AddPrqxLogging(BuilderType.WebAppBff);
+    builder.Services.AddPrqxHealthChecks();
+    builder.Services.AddPrqxApiVersioning();
+    builder.Services.AddPrqxSwagger("Magic Agent API");
+    builder.Services.AddPrqxCors();
+
+    builder.Services
+        .AddControllers()
+        .AddJsonOptions(options =>
+        {
+            if (!options.JsonSerializerOptions.Converters.Any(converter => converter is JsonStringEnumConverter))
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+            }
+        });
+
+    builder.Services.AddPrqxAuthentication();
+    builder.Services.AddPrqxAuthorization(BuilderType.WebAppBff);
+
+    builder.Services.Configure<AgentDefinitionsOptions>(builder.Configuration.GetSection("AgentDefinitions"));
+    builder.Services.AddSingleton<IAgentDefinitionsProvider, FileAgentDefinitionsProvider>();
+    builder.Services.AddSingleton<IAgentDefinitionValueResolver, AgentDefinitionConfigurationResolver>();
+    builder.Services.AddSingleton<IAgentConversationStore, InMemoryAgentConversationStore>();
+    builder.Services.AddSingleton<IAgentDiagnosticsStore, InMemoryAgentDiagnosticsStore>();
+    builder.Services.AddSingleton<IAgentRunProgressSink, NoOpAgentRunProgressSink>();
+    builder.Services.AddSingleton<IAgentRunner, DefaultAgentRunner>();
+    builder.Services.AddWorkflowExpressionServices();
+
+    var app = builder.Build();
+
+    app.UsePrqxConfiguration();
+    app.UsePrqxExceptionHandler();
+    app.UsePrqxHealthChecks();
     app.UsePrqxSwagger();
+
+    app.UseRouting();
+    app.UseCors(CorsConstants.PolicyAdmin);
+    app.UsePrqxAuthorization();
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseCors(CorsConstants.PolicyAdmin);
-app.UsePrqxAuthorization();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An error occurred during bootstrap.");
+}
+finally
+{
+    Log.CloseAndFlush();
+    Thread.Sleep(2000);
+}
 
 public partial class Program;
